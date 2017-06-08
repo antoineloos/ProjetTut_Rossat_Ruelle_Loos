bc36ef011c4aa862fde98aceab5f28a16118b570
totone69 <antoine.almp@hotmail.fr>
2017-06-08 16:34:03 +0200

    utf8 added

--- NetArticlesRest/build/generated-sources/ap-source-output/dal/Article_.java
+++ NetArticlesRest/build/generated-sources/ap-source-output/dal/Article_.java
@@ -10,7 +10,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-06T13:06:54")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-07T10:54:09")
 @StaticMetamodel(Article.class)
 public class Article_ { 
 
