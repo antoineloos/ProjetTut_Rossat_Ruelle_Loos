b420f1a6e7c34aa7aa8c84519d361ee2e9e7b4e6
Alexis <Epulapp@EPULAPP68.univ-lyon1.fr>
2017-06-13 10:37:00 +0200

    fix merge

--- NetArticlesWeb/NetArticlesWeb-war/src/java/controleurs/slCommande.java
+++ NetArticlesWeb/NetArticlesWeb-war/src/java/controleurs/slCommande.java
@@ -42,11 +42,8 @@ public class slCommande extends HttpServlet {
     private AcheteFacade acheteF;
     @EJB
     private DomaineFacade domaineF;
-<<<<<<< HEAD
-=======
     @EJB
     private CompteFacade compteF;
->>>>>>> 74e3406e3321ca3aeb50cbff001f97071d5ada18
 
     private String erreur;
     private List<Article> lstArticleBySelectedDomaine;
@@ -232,14 +229,10 @@ public class slCommande extends HttpServlet {
 
             ArrayList<Article> pan = ((ArrayList<Article>) session.getAttribute("panier"));
 
-<<<<<<< HEAD
             for(int i = 0;i<pan.size();i++){
                 if(Objects.equals(pan.get(i).getIdArticle(), art.getIdArticle())) pan.remove(i);
             }
-            //pan.remove(art);
-=======
-            pan.remove(art);
->>>>>>> 74e3406e3321ca3aeb50cbff001f97071d5ada18
+
             //session.setAttribute("panier", pan);
             request.setAttribute("montantTotalR", ComputeTotal(pan));
             request.setAttribute("lArticlesPanierR", pan);
@@ -255,19 +248,22 @@ public class slCommande extends HttpServlet {
             HttpSession session = request.getSession(true);
             Integer id = (Integer) session.getAttribute("userId");
             ArrayList<Article> pan = ((ArrayList<Article>) session.getAttribute("panier"));
-<<<<<<< HEAD
-            Integer totalPanier = (int) ComputeTotal(pan);
 
-            return ("");
-=======
+    
+
             List<Achete> listeAchats = acheteF.getAcheteByCustomer(id);
 
             ArrayList<Article> tmp = (ArrayList<Article>) ((ArrayList<Article>) pan).clone();
-
+            
+            int i = 0;
             for (Article a : tmp) {
-                if (listeAchats.stream().anyMatch(art -> art.getArticle().getIdArticle() == a.getIdArticle())) {
-                    pan.remove(a);
+//                if (listeAchats.stream().anyMatch(art -> art.getArticle().getIdArticle() == a.getIdArticle())) {
+//                    pan.remove(a);
+//                }
+                for(Achete achete : listeAchats){
+                    if(achete.getArticle().getIdArticle() == a.getIdArticle()) pan.remove(i);
                 }
+                i++;
             }
 
             Integer tot = (int) ComputeTotal(pan);
@@ -282,11 +278,13 @@ public class slCommande extends HttpServlet {
                     }
                 }
             }
+
             Integer res =  acheteF.getAcheteByCustomer(id).size();
             System.out.println(String.valueOf(res));
             request.setAttribute("lAchetesR", acheteF.getAcheteByCustomer(id));
+
             return ("listeAchats.jsp");
->>>>>>> 74e3406e3321ca3aeb50cbff001f97071d5ada18
+
         } catch (Exception e) {
             throw e;
         }
